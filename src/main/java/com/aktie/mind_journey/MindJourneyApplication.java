78a14e1c8acd1bc3eea1da42001cc42069237880
Sandrolaxx <sandro--ramos@hotmail.com>
2024-07-08 22:21:19 -0300

    first commit

---
+++ src/main/java/com/aktie/mind_journey/MindJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.aktie.mind_journey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aktie/mind_journey/MindJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktie.mind_journey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
